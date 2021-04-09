6e2b0355e27e3f00abb1174a06777e4885616753
Maël Gangloff <contact@maelgangloff.fr>
2021-04-09 23:09:27 +0200

    Add BMP280 (pressure) sensor

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ Mesure de:
   - Altitude
   - Température [V0]
   - Hygrométrie relative  [V1]
+  - Pression relative     [V6]
   - Masse par unité de volume d'air de particules fines de 2.5 microns  [V2]
   - Masse par unité de volume d'air de particules fines de 10 microns   [V3]
   - Concentration en monoxyde de carbone  [V4]
@@ -31,6 +32,8 @@ Mesure de:
 
 #include <MQUnifiedsensor.h>
 
+#include <Adafruit_BMP280.h>
+
 #define SD_CS 2
 #define DHT22_PIN 4
 
@@ -40,10 +43,6 @@ Mesure de:
 #define         Voltage_Resolution      (3.3)
 #define         ADC_Bit_Resolution      (12)
 #define         RatioMQ9CleanAir        (9.6)
-MQUnifiedsensor MQ9(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
-
-
-float p10, p25, t, rh, ppm = 0;
 
 SDS011 my_sds(Serial);
 File myFile;
@@ -51,6 +50,11 @@ TinyGPSPlus gps;
 DHT dht(DHT22_PIN, DHT22);
 BlynkTimer timer;
 WidgetLED led1(V5);
+MQUnifiedsensor MQ9(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
+Adafruit_BMP280 bmp; // I2C
+
+float p10, p25, t, rh, press, ppm = 0;
+
 
 static void smartDelay(unsigned long ms)
 {
@@ -68,11 +72,12 @@ static void smartDelay(unsigned long ms)
 *   - Altitude
 *   - Température
 *   - Hygrométrie relative
+*   - Pression relative
 *   - Masse par unité de volume d'air de particules fines de 2.5 microns
 *   - Masse par unité de volume d'air de particules fines de 10 microns
 *   - Concentration en monoxyde de carbone
 */
-void writeFile(TinyGPSPlus gps, float t, float h, float &p25, float &p10, float &ppm) {
+void writeFile(TinyGPSPlus gps, float t, float h, float &press, float &p25, float &p10, float &ppm) {
   char GPSdate[32], GPStime[32];
   sprintf(GPSdate, "%02d/%02d/%02d ", gps.date.day(), gps.date.month(), gps.date.year());
   sprintf(GPStime, "%02d:%02d:%02d ", gps.time.hour(), gps.time.minute(), gps.time.second());
@@ -86,6 +91,7 @@ void writeFile(TinyGPSPlus gps, float t, float h, float &p25, float &p10, float 
                    String(gps.altitude.meters(), 0) + "," +
                    String(t, 2) + "," +
                    String(h, 2) + "," +
+                   String(press, 0) + "," +
                    String(p25, 2) + "," +
                    String(p10, 2) + "," +
                    String(ppm, 2));
@@ -99,8 +105,10 @@ void readAndSend(){
   Blynk.virtualWrite(V2, p25);
   Blynk.virtualWrite(V3, p10);
   Blynk.virtualWrite(V4, ppm);
+  Blynk.virtualWrite(V6, press);
+  Serial.println(press);
   if (gps.location.isValid()) {
-    writeFile(gps, t, rh, p25, p10, ppm);
+    writeFile(gps, t, rh, press, p25, p10, ppm);
     led1.on();
   }else{
     led1.off();
@@ -113,13 +121,13 @@ void setup()
   Serial2.begin(9600); //GPS
 
   SD.begin(SD_CS);
-
   dht.begin();
+  bmp.begin(0x76);
 
   //Si le fichier CSV n'existe pas, on le crée et on insert les en-têtes
   if (!SD.exists("/data.csv")) {
     myFile = SD.open("/data.csv", FILE_APPEND);
-    myFile.println("date,time,lat,lng,alt,temp,hyg,p25,p10,ppm");
+    myFile.println("date,time,lat,lng,alt,temp,hyg,press,p25,p10,ppm");
     myFile.close();
   }
   Blynk.begin(BLYNK_AUTH, WIFI_SSID, WIFI_KEY);
@@ -149,7 +157,8 @@ void loop()
   t = dht.readTemperature();                              //Lecture de la température
   rh = dht.readHumidity();                                //Lecture de l'hygrométrie relative
   MQ9.update();
-  ppm = MQ9.readSensor();
+  ppm = MQ9.readSensor();                                 // Lecture de la concentration en CO
+  press = bmp.readPressure()/100;
 
   timer.run();
   smartDelay(100);
